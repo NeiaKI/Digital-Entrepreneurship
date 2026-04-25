69b05f1262e8eeedae31c69204f73f20267c4a89
NEki <febieki562@gmail.com>
2026-04-25 16:27:24 +0700

    fix: move outputFileTracingExcludes out of experimental in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    outputFileTracingExcludes: {
-      "*": ["3D-ASSET/**/*"],
-    },
+  outputFileTracingExcludes: {
+    "*": ["3D-ASSET/**/*"],
   },
 };
 
